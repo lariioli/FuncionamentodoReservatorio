0e5c3d5efadec7099c05f2ae0628ecc790d84abc
larissa de oliveira <larissaoliveira3zb@gmail.com>
2024-10-03 15:18:16 -0300

    Boia

--- Program.cs
+++ Program.cs
@@ -14,3 +14,17 @@ public class Program
           
         Console.WriteLine("Informe o estado da boia 3 (0 ou 1): ");
         boia3 = int.Parse(Console.ReadLine());
+        if (boia1 == 0 || boia2 == 0 || boia3 == 0)
+        {
+            Console.WriteLine("Ligar válvula e deligar a bomba.");
+        }
+
+        else if (boia1 == 0 || boia2 == 0 || boia3 == 1)
+        {
+            Console.WriteLine("Ligar válvula e desligar bomba.");
+        }
+
+        else if (boia1 == 0 || boia2 == 1 || boia3 == 0)
+        {
+            Console.WriteLine("Falha na válvula ou na bomba.");
+        }
