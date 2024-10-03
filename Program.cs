000d2dc47a62e787eaf87deef84ebbc84602b70f
larissa de oliveira <larissaoliveira3zb@gmail.com>
2024-10-03 15:17:18 -0300

    Boia

--- Program.cs
+++ Program.cs
@@ -6,3 +6,11 @@ public class Program
     {
         int boia1, boia2, boia3;
         bool valvulaLigada, bombaLigada;
+        Console.WriteLine("Informe o estado da boia 1 (0 ou 1): ");
+        boia1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Informe o estado da boia 2 (0 ou 1): ");
+        boia2 = int.Parse(Console.ReadLine());
+          
+        Console.WriteLine("Informe o estado da boia 3 (0 ou 1): ");
+        boia3 = int.Parse(Console.ReadLine());
